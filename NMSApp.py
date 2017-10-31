c83121645fa05724f189332e1bffbac499638df8
ubaumann <git@m.ubaumann.ch>
2017-10-31 19:14:36 +0100

    Solution with NAPALM for CSR1000v

--- NMSApp.py
+++ NMSApp.py
@@ -1,39 +1,43 @@
-from flask import Flask, render_template, request
+import string
+
+import napalm_base
+from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
+driver = napalm_base.get_network_driver('ios')
+username = 'ins'
+password = 'ins'
+
 
 @app.route('/')
-def hello_world():
-    routers = ['152.96.9.201', '152.96.9.202', '152.96.9.203']
+def welcome():
+    routers = ['152.96.9.77', '152.96.9.79', '152.96.9.90', '152.96.9.91']
     return render_template('index.html', routers=routers)
 
 
 @app.route('/interface_status/<router>')
 def status(router):
-    # TODO get status of all interfaces
-    interfaces = {'items':
-        [
-            {
-                "kind": "object#interface",
-                "type": "ethernet",
-                "if-name": "gigabitEthernet1",
-                "description": "management interface",
-                "ip-address": "129.10.10.10",
-                "subnet-mask": "255.255.254.0"
-            }
-        ]
-    }
+    device = driver(hostname=router, username=username, password=password)
+    device.open()
+    interfaces = device.get_interfaces()
+    interfaces_ip = device.get_interfaces_ip()
+    device.close()
+
+    for name in interfaces_ip:
+        try:
+            interfaces[name]['ipv'] = interfaces_ip[name]['ipv4']
+        except KeyError:
+            interfaces[name]['ipv'] = 'no ip address'
 
     return render_template('status.html', router=router, interfaces=interfaces)
 
 
 @app.route('/show_config/<router>')
 def show_config(router):
-    # TODO get running config of device
-    config = ['hostname router.ins.hsr.ch',
-              'interface GigabitEthernet1',
-              '  ip address 10.1.1.1 255.0.0.0']
+    device = driver(hostname=router, username=username, password=password)
+    device.open()
+    config = device.get_config()['startup'].split(string.punctuation)
     return render_template('show_config.html', router=router, config=config)
 
 
